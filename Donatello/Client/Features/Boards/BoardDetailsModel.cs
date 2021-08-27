8f6ceb450e1639614c1eda7dc071fd854eacb71f
Jon Hilton <hilton.jon@gmail.com>
2021-08-27 11:18:16 +0100

    Added new component to show a board (details) plus corresponding model

---
+++ Donatello/Client/Features/Boards/BoardDetailsModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Donatello.Client.Features.Boards;
+public class BoardDetailsModel
+{
+    public List<Column> Columns { get; set; } = new List<Column>();
+
+    public class Column
+    {
+        public string Title { get; set; }
+    }
+}
