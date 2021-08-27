0d79f9324a8d6c0c0c1b1ef781c6ea177d815bfd
Jon Hilton <hilton.jon@gmail.com>
2021-08-27 14:01:50 +0100

    Updated board details component to display cards from model

--- Donatello/Client/Features/Boards/BoardDetailsModel.cs
+++ Donatello/Client/Features/Boards/BoardDetailsModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 
 namespace Donatello.Client.Features.Boards;
+
 public class BoardDetailsModel
 {
     public List<Column> Columns { get; set; } = new List<Column>();
@@ -8,5 +9,11 @@ public class BoardDetailsModel
     public class Column
     {
         public string Title { get; set; }
+        public List<Card> Cards { get; set; } = new List<Card>();
+    }
+    
+    public class Card
+    {
+        public string Content { get; set; }
     }
 }
